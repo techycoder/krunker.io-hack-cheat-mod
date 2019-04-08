c80d093b21909d6b878c54f3f6a3360b51d57e69
MrCoderN <45430918+MrCoderN@users.noreply.github.com>
2019-04-08 03:15:35 +0300

    Update krunkerhack.user.js

--- krunkerhack.user.js
+++ krunkerhack.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Krunker.io 2019 Aimbot (Hacks,Mods,Cheats) | NO DISCONNECT ISSUE | KRUNKERIO.ORG
-// @version      6.4
+// @version      6.5
 // @description  Aimbot, Unlimited Ammo, Auto Heal, ESP, Wall Hack, Unlimited Ammo... -2019 krunkerio hack- ADBLOCK
 // @author       MR.Coder
 // @namespace MR.Coder
